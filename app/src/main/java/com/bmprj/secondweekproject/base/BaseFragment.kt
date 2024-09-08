8f0372afcb499ec5c1f8c8674252140f45f33fa4
Busra <busramacak@outlook.com>
2024-09-09 01:30:56 +0300

    Added viewBinding to build.gradle and implemented BaseFragment structure using viewBinding.

---
+++ app/src/main/java/com/bmprj/secondweekproject/base/BaseFragment.kt
@@ -0,0 +1,32 @@
+package com.bmprj.secondweekproject.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<VB: ViewBinding>(
+    private val bindingInflater:(LayoutInflater) -> VB
+) : Fragment() {
+
+    private lateinit var _binding:VB
+    protected val binding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = bindingInflater.invoke(inflater)
+        return _binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViews()
+    }
+
+    abstract fun setupViews()
+}
