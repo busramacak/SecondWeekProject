b5cc5d79b19a6a4f9fccad309f9d521c2d4f1703
Busra <busramacak@outlook.com>
2024-09-09 22:34:44 +0300

    Designed and added DetailFragment to the project

---
+++ app/src/main/java/com/bmprj/secondweekproject/ui/detail/DetailFragment.kt
@@ -0,0 +1,11 @@
+package com.bmprj.secondweekproject.ui.detail
+
+import com.bmprj.secondweekproject.base.BaseFragment
+import com.bmprj.secondweekproject.databinding.FragmentDetailBinding
+
+class DetailFragment : BaseFragment<FragmentDetailBinding>(FragmentDetailBinding::inflate) {
+    override fun setupViews() {
+
+    }
+
+}
--- app/src/main/java/com/bmprj/secondweekproject/ui/learnedList/LearnedWordsFragment.kt
+++ app/src/main/java/com/bmprj/secondweekproject/ui/learnedList/LearnedWordsFragment.kt
@@ -1,11 +1,6 @@
 package com.bmprj.secondweekproject.ui.learnedList
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.bmprj.secondweekproject.R
+
 import com.bmprj.secondweekproject.base.BaseFragment
 import com.bmprj.secondweekproject.databinding.FragmentLearnedWordsBinding
 
