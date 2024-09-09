4b58fc9cb488f36fc149a09ae418f648a3671644
Busra <busramacak@outlook.com>
2024-09-09 22:16:52 +0300

    Set up BottomNavigation interaction with MainActivity

--- app/src/main/java/com/bmprj/secondweekproject/ui/MainActivity.kt
+++ app/src/main/java/com/bmprj/secondweekproject/ui/MainActivity.kt
@@ -3,12 +3,28 @@ package com.bmprj.secondweekproject.ui
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.bmprj.secondweekproject.R
+import com.bmprj.secondweekproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        NavigationUI.setupWithNavController(
+            binding.bottomNavigationView,
+            navHostFragment.navController
+        )
     }
 }
